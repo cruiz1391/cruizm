e3091d7cdb8ad9b8011a80097c9112c7164c8b3b
Carlos <carlosr@boxycharm.com>
2017-11-17 16:44:38 -0500

    optm

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-appcfg.py -A cruizm update ${GOPATH}/src/github.com/crui1391/cruizm/
+appcfg.py -A cruizm-185917 update ${GOPATH}/src/github.com/cruiz1391/cruizm
--- routes.go
+++ routes.go
@@ -2,6 +2,9 @@ package cruizm
 
 import (
 	"net/http"
+	"time"
+	"strings"
+	"fmt"
 )
 
 func init()  {
@@ -9,12 +12,40 @@ func init()  {
 	http.HandleFunc("/static/", assetsHandler)
 }
 
+var (
+	cacheSince = time.Now().Format(http.TimeFormat)
+	cacheUntil = time.Now().AddDate(0, 0, 1).Format(http.TimeFormat)
+)
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.Host, "www") {
+		httpHeader := "http"
+		if r.TLS != nil {
+			httpHeader = "https"
+		}
+		newUrl := fmt.Sprintf("%s://www.%s%s", httpHeader, r.Host, r.URL.Path)
+		if r.URL.RawQuery != "" {
+			newUrl += "?" + r.URL.RawQuery
+		}
+		http.Redirect(w, r, newUrl, http.StatusMovedPermanently)
+		return
+	}
+
+	w.Header().Set("Cache-Control", "max-age:86400, public")
+	w.Header().Set("Last-Modified", cacheSince)
+	w.Header().Set("Expires", cacheUntil)
+
 	if r.URL.Path=="/" {
 		http.ServeFile(w,r,"index.html")
 		return
+	} else if r.URL.Path=="/robots.txt" {
+		http.ServeFile(w,r,"robots.txt")
+		return
+	} else if r.URL.Path=="/sitemap.xml" {
+		http.ServeFile(w,r,"sitemap.xml")
+		return
 	}
-	http.Redirect(w,r,"/", http.StatusTemporaryRedirect)
+	http.Redirect(w,r,"/", http.StatusMovedPermanently)
 }
 
 func assetsHandler(w http.ResponseWriter, r *http.Request){
